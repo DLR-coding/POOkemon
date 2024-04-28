5d8bdb2cdc8695b83c21b1c01d49697feb2df1da
Eliot <eliot.urffer@gmail.com>
2024-04-28 22:31:41 +0200

    code eliot

---
+++ src/Pokemon2.java
@@ -0,0 +1,47 @@
+public class pokemon
+{
+    private String m_nom;
+    private element m_elements;
+    private int m_vie;
+    private int m_attaque;
+
+    public pokemon(String nom,element element,int vie,int attaque)
+    {
+
+       m_nom = nom;
+       m_elements = element;
+       m_vie = vie;
+       m_attaque =attaque;
+    }
+
+public void Attaquer(pokemon blesser)
+{
+    int attaque = 0;
+    if (m_elements.Getforcontre() == blesser.m_elements.toString()) {
+        attaque = 10;
+    }
+    else if (m_elements.Getfaible() == blesser.m_elements.toString()) {
+        attaque = -10;
+    }
+    attaque = attaque + this.m_attaque;
+    blesser.m_vie = blesser.m_vie - attaque ;
+    System.out.println(m_nom + " attaque de " + attaque + " a été effectué a "+ blesser.toString());
+
+}
+
+
+
+    public int getVie() {
+        return this.m_vie;
+    }
+
+    public int getM_attaque() {
+        return m_attaque;
+    }
+
+    @Override
+    public String toString() {
+        return m_nom;
+    }
+}
+
